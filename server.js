1267ecbbe2f01f01c66460aada08b51ab79db533
SILENTIUM <mr.schurenkov@gmail.com>
2018-07-24 17:27:34 +0300

    fix

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var express = require('express');
 var app = express();
 const path = require('path');
 
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+app.use(express.static('build'));
 app.listen(process.env.PORT || 8080);
