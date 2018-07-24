e5b2281829e7d0daf3b41e4d6d052671f8adc630
SILENTIUM <mr.schurenkov@gmail.com>
2018-07-24 17:29:28 +0300

    fix

--- src/api/main-api.js
+++ src/api/main-api.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 import store from '../store';
 
 export function getDialogs(){
-  return axios.get('../build/data/data.json')
+  return axios.get('../data/data.json')
               .then(result=> { return result.data })
 }
